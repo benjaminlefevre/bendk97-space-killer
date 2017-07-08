9573e5045f7cb229adf3d89f3e145ca19bddca34
Benjamin Lefevre <benjamin.lefevre@ejust.fr>
2017-07-08 17:30:27 +0200

    init project

---
+++ android/src/com/benk97/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.benk97;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.benk97.SpaceKillerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceKillerGame(), config);
+	}
+}
