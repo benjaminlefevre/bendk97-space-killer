33e5adadc148c1d2ee8d7a3e314cfc11572d957d
Benjamin Lefevre <benjamin.lefevre.github@gmail.com>
2018-10-16 09:08:53 +0200

    add effect to logo in spashscreen

--- spaceKiller/src/main/java/com/bendk97/tweens/SpriteTween.java
+++ spaceKiller/src/main/java/com/bendk97/tweens/SpriteTween.java
@@ -10,7 +10,9 @@ import aurelienribon.tweenengine.TweenAccessor;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class SpriteTween implements TweenAccessor<Sprite> {
-    public final static int POS_XY = 2;
+    public final static int POS_XY = 0;
+    public final static int ROTATION = 1;
+    public final static int SCALE = 2;
 
     @Override
     public int getValues(Sprite target, int type, float[] returnValues) {
@@ -18,6 +20,13 @@ public class SpriteTween implements TweenAccessor<Sprite> {
             case POS_XY:
                 returnValues[0] = target.getX();
                 returnValues[1] = target.getY();
+                return 2;
+            case ROTATION:
+                returnValues[0] = target.getRotation();
+                return 1;
+            case SCALE:
+                returnValues[0] = target.getScaleX();
+                returnValues[1] = target.getScaleY();
                 return 2;
             default:
                 return -1;
@@ -30,6 +39,12 @@ public class SpriteTween implements TweenAccessor<Sprite> {
             case POS_XY:
                 target.setX(newValues[0]);
                 target.setY(newValues[1]);
+                break;
+            case ROTATION:
+                target.setRotation(newValues[0]);
+                break;
+            case SCALE:
+                target.setScale(newValues[0], newValues[1]);
                 break;
             default:
                 break;
